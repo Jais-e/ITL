4294bbc94f36f9066c1f6e69ebe37b2ba70fa3d9
Jais Kurecko Eriksen <eaajaeri6@students.eaaa.dk>
2019-05-06 20:29:11 +0200

    ...

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 let visible = false;
 let consent = false;
-
+let maxWidth = 480;
+let winWidth = window.innerWidth;
 document.querySelector("#showMore").onclick = function() {
  toggle();
 };
@@ -21,10 +22,49 @@ function toggle(){
     visible = false
   }
 };
+document.querySelector("#tab2").onclick = function(){
+  activeTab();
+  document.querySelector("#tab2").style.borderTopLeftRadius ="5px";
+  document.querySelector("#nemId").style.display = "none";
+  document.querySelector("#login").style.display = "block";
+
+
+};
+document.querySelector("#tab1").onclick = function(){
+  activeTab();
+  document.querySelector("#login").style.display = "none";
+
+  if (winWidth < maxWidth){
+    document.querySelector("#nemId").style.display = "block";
+
+  }else
+  {
+    document.querySelector("#nemId").style.display = "flex";
+  }
+};
+
+function activeTab(){
+  let tab1 = document.getElementById("tab1").className;
+  let tab2 = document.getElementById("tab2").className;
+  if (tab1 != "tab active" || tab2 === "tab active"){
+    document.getElementById("tab1").className = "tab active";
+    document.getElementById("tab2").className = "tab"
+    document.getElementById("tab1").style.pointerEvents = "none";
+    document.getElementById("tab2").style.pointerEvents = "auto";
+  }
+  else if (tab1 === "tab active"){
+    document.getElementById("tab1").className = "tab"
+    document.getElementById("tab2").className = "tab active";
+    document.getElementById("tab2").style.pointerEvents = "none";
+    document.getElementById("tab1").style.pointerEvents = "auto";
+  }
+};
+
 // Web storage
 document.querySelector("#cookie-consent").onclick = function(){
 consent = true;
   console.log(consent);
+  activeTab();
   // store value in local storage
   localStorage.setItem("boolean", consent);
   // call loadFromStorage to update displayed values
@@ -39,4 +79,5 @@ function loadFromStorage() {
 loadFromStorage();
 if (consent== "true"){
   document.querySelector("#cookie").style.display = "none";
+  activeTab();
 };
